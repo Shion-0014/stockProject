c2dc8596d17ae9b34cc16e9d7f9673cb5f988a00
Shion-0014 <seanmqe@gmail.com>
2019-10-11 22:49:02 +0900

    ADD: line notify

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
+from time import sleep
 import pandas as pd
 from utils.strategy_index_functions import comp_rsi, comp_sma
-from settings import DATAFILE, DIC_endfrag, DIC_k
+from utils.line_notify import LINENotifyBot
+from settings import DATAFILE, DIC_endfrag, DIC_k, ACCESS_TOKEN
 
 
 if __name__ == '__main__':
@@ -12,23 +14,35 @@ if __name__ == '__main__':
     print('1min, 5min, 15min, 30min, 1h, 1d, 1w, 1m')
     TIME_LEG = str(input())
 
+    print('SMAの条件を指定してください.')
+    SMA_TH = int(input())
+    print('RSIの条件を指定してください.')
+    RSI_TH = int(input())
+
+    # linebotインスタンスを生成
+    bot = LINENotifyBot(access_token=ACCESS_TOKEN)
+
     # データの数を計算
     num_data = int(N / DIC_k[TIME_LEG])
 
-    # dataの取得
-    data = pd.read_csv(DATAFILE)
+    while True:
+        # dataの取得
+        data = pd.read_csv(DATAFILE)
 
-    # 現在の値を取得
-    data_now = data.iloc[-1, :]
+        # 現在の値を取得
+        data_now = data.iloc[-1, :]
 
-    # 時間分解能に対応する, 各足の終値を取得
-    data = data[data[DIC_endfrag[TIME_LEG]] == 1]
-    # 末尾が一致していないなら現在のものを加える。一致しているならそのままでいい。
-    if data_now[DIC_endfrag[TIME_LEG]] != 1:
-        data = pd.concat([data, pd.DataFrame(data_now).T], axis=0)
+        # 時間分解能に対応する, 各足の終値を取得
+        data = data[data[DIC_endfrag[TIME_LEG]] == 1]
+        # 末尾が一致していないなら現在のものを加える。一致しているならそのままでいい。
+        if data_now[DIC_endfrag[TIME_LEG]] != 1:
+            data = pd.concat([data, pd.DataFrame(data_now).T], axis=0)
 
-    SMA = comp_sma(data, num_data)
-    RSI = comp_rsi(data, num_data)
+        sma = comp_sma(data, num_data)
+        rsi = comp_rsi(data, num_data)
 
-    print(SMA, RSI)
+        # Lineに通知
+        if sma > SMA_TH and rsi > RSI_TH:
+            bot.send(message='株価が条件値を満たしました')
 
+        sleep(10)
--- src/settings.py
+++ src/settings.py
@@ -5,5 +5,5 @@ DIC_k = {'1min': 1, '5min':5, '15min':15, '30min':30, '1h':60, '1d':24 * 60, '1w
 DIC_endfrag = {'1min': '1min_endfrag', '5min': '5min_endfrag', '15min':'15min_endfrag', '30min':'30min_endfrag', '1h':'1h_endfrag',
                '1d':'1d_endfrag', '1w':'1w_endfrag', '1m':'1m_endfrag'}
 
-
+ACCESS_TOKEN = 'MKhPH3xvovoorS1TVpampxcL13dw7whRUylyVOn2XJ2'
 
